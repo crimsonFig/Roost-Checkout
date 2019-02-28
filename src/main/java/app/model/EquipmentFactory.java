ad3606b1342bef890639ff35db56b8e09657e0a9
Triston Scallan <triston.scaln@icloud.com>
2019-02-28 00:22:32 -0600

    (feat) add model factories
    * added model factories to help lay groundwork for future model instantiation. using a factory can help ensure uniform and valid instantiation using a system such as XML files or a map object without cluttering the model class and allowing modular changes to the system.

---
+++ src/main/java/app/model/EquipmentFactory.java
@@ -0,0 +1,11 @@
+package app.model;
+
+/**
+ * factory class for the equipment model. used to create and initialize equipment in a uniform manner.
+ */
+class EquipmentFactory {
+
+    static Equipment initFactory(Equipment equipment) {
+        return equipment;
+    }
+}
