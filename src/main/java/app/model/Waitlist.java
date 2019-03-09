5842a06182edd397eb9b73469aaf5c4a97c29d99
Triston Scallan <triston.scaln@icloud.com>
2019-03-09 05:52:20 -0600

    (refactor) renames mockdata classes
    * renames "outentry" and "waitlistentry" into session and waitlist for preparation of the respective domain models.

---
+++ src/main/java/app/model/Waitlist.java
@@ -0,0 +1,71 @@
+package app.model;
+
+import java.util.ArrayList;
+
+import javafx.beans.property.StringProperty;
+import javafx.collections.ObservableList;
+
+public class Waitlist {
+
+	private int time;
+	private String name, equipmentString, stationName;
+	private Station station;
+	private ObservableList<StringProperty> equipment;
+	
+	public Waitlist(String name, Station station) {
+		this.name = name;
+		this.station = station;
+		setEquipment(station.getEquipmentGroups());//temp
+		time = 0;
+	}
+	
+	public Waitlist(String name, String station) {
+		this.name = name;
+		this.stationName = station;
+		time = 0;
+	}
+	
+	public int getTime() {
+		return time;
+	}
+	public void setTime(int time) {
+		this.time = time;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Station getStation() {
+		return station;
+	}
+	public void setStation(Station station) {
+		this.station = station;
+	}
+	public ObservableList<StringProperty> getEquipment(){
+		return equipment;
+	}	
+	public void setEquipment(ObservableList<StringProperty> observableList) {
+		this.equipment = observableList;
+		setEquipmentString("");
+		for(StringProperty e : observableList)
+			setEquipmentString(getEquipmentString() + e + "\n");
+	}
+
+	public String getEquipmentString() {
+		return equipmentString;
+	}
+
+	public void setEquipmentString(String equipmentString) {
+		this.equipmentString = equipmentString;
+	}
+
+	public String getStationName() {
+		return stationName;
+	}
+
+	public void setStationName(String stationName) {
+		this.stationName = stationName;
+	}
+}
